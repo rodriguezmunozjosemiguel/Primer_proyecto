7769dc3741e2c31d51b92a727784a08b7a4a7f4e
rodriguezmunozjosemiguel <jrodmun681@g.educaand.es>
2021-10-17 17:55:30 +0200

    first commit

---
+++ src/películas.js
@@ -0,0 +1,18 @@
+db.películas.insertMany([
+    {película: "Titanic" , género:"clásico",valoración:8},
+    {película: "El cantor de jazz" , género:"sonoro",valoración:7},
+    {película: "Rey León" , género:"infantil",valoración:9},
+    {película: "El Padrino" , género:"crimen",valoración:8.9},
+    {película: "Trainspotting" , género:"adultos",valoración:7},
+    {película: "Stars Wars" , género:"ciencia ficción",valoración:9.7},
+    {película: "Mary Poppins" , género:"musical",valoración:10},
+    {película: "Expediente Warren" , género:"terror",valoración:9},
+    {película: "Ladrón de bicicletas" , género:"dramática ",valoración:7},
+    {película: " ¿Quién son los Miller?" , género:"comedia ",valoración:8.9},
+    {película: "Las Aventuras de Indiana Jones" , género:"aventura",valoración:9},
+    {película: "Vengadores: Infinity War" , género:"acción",valoración:8.7},
+    {película: "Nosferatu" , género:"blanco y negro",valoración:8.1},
+    {película: "Luces de la ciudad" , género:"mudas",valoración:7.6},
+    {película: "La propuesta" , género:"oeste",valoración:9.2}
+])
+   
